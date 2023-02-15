e982df2c714b9b2d42d6380e2b42dbc05e2b80f8
ubuntu_profile <rzenhier@gmail.com>
2023-02-15 07:39:24 +0000

    Update dist/ pyproject.toml Rename finddiff/ > gendiff/

--- gendiff/fdiff_help.py
+++ gendiff/fdiff_help.py
@@ -4,12 +4,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-    prog = 'finddiff',
+    prog = 'gendiff',
     description = 'Compares two configuration files and shows a difference.')
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
+    parser.add_argument('-f', '--format', help = 'set format of output')
     args = parser.parse_args()
 
 if __name__ == "__main__":
