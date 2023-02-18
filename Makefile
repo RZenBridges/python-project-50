d4dce723a58c44ac2d5cff8e1145c0a197f51103
ubuntu_profile <rzenhier@gmail.com>
2023-02-18 03:31:08 +0000

    Update .github/ Makefile gendiff/ pyproject.toml setup.cfg tests/

--- Makefile
+++ Makefile
@@ -13,5 +13,5 @@ package-install:
 lint:
 	poetry run flake8 gendiff
 
-tests:
+testing:
 	poetry run pytest
--- gendiff/operations.py
+++ gendiff/operations.py
@@ -5,28 +5,32 @@ def path_to_json(file_path):
     return json.load(open(file_path))
 
 
-def generate_diff(file_path1, file_path2):
+def diff_check(json1, json2):
     difference = "{\n"
-    json1 = path_to_json(file_path1)
-    json2 = path_to_json(file_path2)
-    key_set1 = list(json1.keys())
-    key_set2 = list(json2.keys())
 
-    for k1 in key_set1:
-        if k1 in key_set2 and json1[k1] == json2[k1]:
+    for k1 in json1:
+        if k1 in json2 and json1[k1] == json2[k1]:
             difference += f"    {k1}: {json1[k1]}\n"
-        elif k1 not in key_set2:
+        elif k1 not in json2:
             difference += f"  - {k1}: {json1[k1]}\n"
-        elif k1 in key_set2 and json1[k1] != json2[k1]:
+        elif k1 in json2 and json1[k1] != json2[k1]:
             difference += f"  - {k1}: {json1[k1]}\n"
-            difference += f"  + {k1}: {json2[k1]}\n"
 
-    for k2 in key_set2:
-        if k2 not in key_set1:
+    for k2 in json2:
+        if k2 not in json1:
+            difference += f"  + {k2}: {json2[k2]}\n"
+        elif k2 in json1 and json2[k2] != json1[k2]:
             difference += f"  + {k2}: {json2[k2]}\n"
 
     difference += "}\n"
-    print(difference)
     return difference
+
+
+def generate_diff(file_path1, file_path2):
+    json1 = path_to_json(file_path1)
+    json2 = path_to_json(file_path2)
+    result = diff_check(json1, json2)
+    print(result)
+    return result
 
 # generate_diff('/home/istari/file1.json', '/home/istari/file2.json')
