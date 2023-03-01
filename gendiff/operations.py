a1558a773413a79b52c175f13f3a7e466a81c16c
RZenBridges <bartellbe@gmail.com>
2023-03-01 06:35:55 +0000

    Update tests/* operations.py dist/

--- gendiff/operations.py
+++ gendiff/operations.py
@@ -1,27 +1,95 @@
 from gendiff.data_parsing import adjust_format
 
 
+def stylish(input):
+    def formating(dictionary, depth):
+        if not isinstance(dictionary, dict):
+            return str(dictionary)
+        sorted_dict = dict(
+            sorted(
+                dictionary.items(),
+                key=lambda item: item[0][4:]
+            )
+        )
+        result = "{\n"
+        current_depth = depth
+        bare_indent = "    " * current_depth
+        current_depth += 1
+        for key, value in sorted_dict.items():
+            if value in ("True", "False"):
+                value = value.lower()
+            if value == "None":
+                value = "null"
+            result += f"{bare_indent}{key}: {formating(value, current_depth)}\n"
+        result += f"{bare_indent}" + "}"
+        return result
+    return formating(input, 0)
+
+
+def low_level_diff_1(k1, value1, value2, depth, match, minus, dif, func):
+    if not isinstance(value2, str) and not isinstance(value1, str):
+        main_cond = k1 in value2
+        cond_1 = value1.get(k1) == value2.get(k1)
+        cond_2 = not cond_1 and isinstance(value2.get(k1), dict)
+        cond_3 = isinstance(value2, dict)
+
+        if main_cond and (cond_1 or cond_2 or not cond_3):
+            dif[f"{match}{k1}"] = func(value1[k1], value2[k1], depth)
+        elif main_cond and not cond_1:
+            dif[f"{minus}{k1}"] = func(value1[k1], value2[k1], depth)
+        elif not main_cond:
+            dif[f"{minus}{k1}"] = func(value1[k1], value1[k1], depth)
+    else:
+        dif[f"{match}{k1}"] = func(value1[k1], value1[k1], depth)
+    return
+
+
+def low_level_diff_2(k2, value1, value2, depth, plus, dif, func):
+    main_cond = k2 in value1
+    if not isinstance(value2, str) and not isinstance(value1, str):
+        cond_1 = k2 in [i[:4] for i in dif.keys()]
+        cond_2 = value1.get(k2) != value2.get(k2)
+        cond_3 = isinstance(value2.get(k2), dict)
+        term_1 = not main_cond and not cond_1
+        term_2 = main_cond and cond_2 and not cond_3
+        if term_1 or term_2:
+            dif[f"{plus}{k2}"] = func(value2[k2], value2[k2], depth)
+    return
+
+
 def diff_check(json1, json2):
-    difference = "{\n"
 
-    for k1 in json1:
-        if k1 in json2 and json1[k1] == json2[k1]:
-            difference += f"    {k1}: {json1[k1]}\n"
-        elif (k1 not in json2) or (k1 in json2 and json1[k1] != json2[k1]):
-            difference += f"  - {k1}: {json1[k1]}\n"
+    def matching(value1, value2, depth):
+        if type(value1) == type(value2):
+            if not isinstance(value1, dict):
+                return str(value1)
+            if not isinstance(value2, dict):
+                return str(value2)
+        else:
+            if not isinstance(value1, dict):
+                return str(value1)
+        difference = {}
+        current_depth = depth
+        match_indent = "    "
+        minus_indent = "  - "
+        plus_indent = "  + "
+        current_depth += 1
 
-    for k2 in json2:
-        if (k2 not in json1) or (k2 in json1 and json2[k2] != json1[k2]):
-            difference += f"  + {k2}: {json2[k2]}\n"
+        for k1 in value1:
+            low_level_diff_1(k1, value1, value2, current_depth,
+                             match_indent, minus_indent, difference, matching)
 
-    difference += "}\n"
-    return difference
+        for k2 in value2:
+            low_level_diff_2(k2, value1, value2, current_depth,
+                             plus_indent, difference, matching)
+        return difference
+    return matching(json1, json2, 0)
 
 
 def generate_diff(file_path1, file_path2):
     content1 = adjust_format(file_path1)
     content2 = adjust_format(file_path2)
-    result = diff_check(content1, content2)
+    result = stylish(diff_check(content1, content2))
     print(result)
     return result
 
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,15 +1,25 @@
 from gendiff.operations import generate_diff
 
 
-def test_generate_diff_1():
-    with open('tests/fixtures/test_result1.txt', 'r') as data:
-         assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == data.read()
+#def test_generate_diff_1():
+#    with open('tests/fixtures/test_result1.txt', 'r') as data:
+#         assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == data.read()
 
 
-def test_generate_diff_2():
-    with open('tests/fixtures/test_result2.txt', 'r') as data:
-        assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file1.json') == data.read()
+#def test_generate_diff_2():
+#    with open('tests/fixtures/test_result2.txt', 'r') as data:
+#        assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file1.json') == data.read()
 
-def test_generate_diff_3():
-    with open('tests/fixtures/test_result3.txt', 'r') as data:
-        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.json') == data.read()
+#def test_generate_diff_3():
+#    with open('tests/fixtures/test_result3.txt', 'r') as data:
+#        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.json') == data.read()
+
+
+def test_generate_diff_recursive_1():
+    with open('tests/fixtures/recursive_result.txt', 'r') as data:
+        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == data.read().strip()
+
+
+def test_generate_diff_recursive_2():
+    with open('tests/fixtures/recursive_result.txt', 'r') as data:
+        assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == data.read().strip()
