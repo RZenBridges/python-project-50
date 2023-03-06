5609138b7436f86c23f145b2022171f234b0e495
RZenBridges <bartellbe@gmail.com>
2023-03-06 16:36:17 +0000

    Update dist/* gendiff/__init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,4 @@
-from gendiff.scripts.gendiff import generate_diff
+from gendiff.scripts.gendiff import generate_diff, stylish, plain, json
 
-__all__ = (generate_diff, )
+
+__all__ = (generate_diff, stylish, plain, json)
