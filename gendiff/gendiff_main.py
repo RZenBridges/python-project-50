6a046c788e6558f48c4bd91e0b048e4c4f1ca722
RZenBridges <bartellbe@gmail.com>
2023-03-21 10:11:05 +0000

    Update scripts.gendiff.py dist/ Remove gendiff_main.py

--- gendiff/gendiff_main.py
+++
@@ -1,8 +0,0 @@
-from gendiff.diff_calc import generate_diff
-from gendiff import cli
-
-
-def call_gendiff():
-    file_1, file_2, format = cli.get_arguments()
-    result = generate_diff(file_1, file_2, format)
-    print(result)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from gendiff.gendiff_main import call_gendiff
+from gendiff import generate_diff, cli
 
 
 def main():
-    call_gendiff()
+    file_1, file_2, format = cli.get_arguments()
+    result = generate_diff(file_1, file_2, format)
+    print(result)
 
 
 if __name__ == "__main__":
