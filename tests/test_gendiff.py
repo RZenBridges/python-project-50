2d14ecdfc5ed0f9ff46421d12198832690f4034a
RZen <bartellbe@gmail.com>
2023-03-30 08:01:47 +0000

    Refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,38 +3,58 @@ from gendiff import generate_diff
 
 
 @pytest.mark.parametrize(
-    'file1,file2,expected,format', [
+    'file1,file2,format,expected_result', [
         ('tests/fixtures/input_files/file1.json',
          'tests/fixtures/input_files/file2.json',
-         'tests/fixtures/format_stylish_12.txt',
-         'stylish'),
+         'stylish',
+         'tests/fixtures/format_stylish_12.txt'),
         ('tests/fixtures/input_files/file5.yml',
          'tests/fixtures/input_files/file6.yml',
-         'tests/fixtures/format_stylish_56.txt',
-         'stylish'),
+         'stylish',
+         'tests/fixtures/format_stylish_56.txt'),
         ('tests/fixtures/input_files/file7.json',
          'tests/fixtures/input_files/file8.json',
-         'tests/fixtures/format_stylish_78.txt',
-         'stylish'),
-        ('tests/fixtures/input_files/file1.json',
-         'tests/fixtures/input_files/file2.json',
-         'tests/fixtures/format_plain_12.txt',
-         'plain'),
-        ('tests/fixtures/input_files/file5.yml',
-         'tests/fixtures/input_files/file6.yml',
-         'tests/fixtures/format_plain_56.txt',
-         'plain'),
-        ('tests/fixtures/input_files/file7.json',
-         'tests/fixtures/input_files/file8.json',
-         'tests/fixtures/format_plain_78.txt',
-         'plain'),
-        ('tests/fixtures/input_files/file1.json',
-         'tests/fixtures/input_files/file2.json',
-         'tests/fixtures/format_json.txt',
-         'json')
+         'stylish',
+         'tests/fixtures/format_stylish_78.txt'),
     ])
-def test_gendiff_main(file1, file2, expected, format):
-    with open(expected) as control:
-        result = control.read().strip()
-    out = generate_diff(file1, file2, format)
-    assert out == result
+def test_gendiff_stylish(file1, file2, format, expected_result):
+    with open(expected_result) as data:
+        expected_diff = data.read().strip()
+    diff = generate_diff(file1, file2, format)
+    assert diff == expected_diff
+
+
+@pytest.mark.parametrize(
+    'file1,file2,format,expected_result', [
+        ('tests/fixtures/input_files/file1.json',
+         'tests/fixtures/input_files/file2.json',
+         'plain',
+         'tests/fixtures/format_plain_12.txt'),
+        ('tests/fixtures/input_files/file5.yml',
+         'tests/fixtures/input_files/file6.yml',
+         'plain',
+         'tests/fixtures/format_plain_56.txt'),
+        ('tests/fixtures/input_files/file7.json',
+         'tests/fixtures/input_files/file8.json',
+         'plain',
+         'tests/fixtures/format_plain_78.txt'),
+    ])
+def test_gendiff_plain(file1, file2, format, expected_result):
+    with open(expected_result) as data:
+        expected_diff = data.read().strip()
+    diff = generate_diff(file1, file2, format)
+    assert diff == expected_diff
+
+
+@pytest.mark.parametrize(
+    'file1,file2,format,expected_result', [
+        ('tests/fixtures/input_files/file1.json',
+         'tests/fixtures/input_files/file2.json',
+         'json',
+         'tests/fixtures/format_json.txt')
+    ])
+def test_gendiff_json(file1, file2, format, expected_result):
+    with open(expected_result) as data:
+        expected_diff = data.read().strip()
+    diff = generate_diff(file1, file2, format)
+    assert diff == expected_diff
