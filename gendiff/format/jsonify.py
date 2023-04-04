95aef17265efee17c9203b0e7dd9d72c331beb5d
RZen <bartellbe@gmail.com>
2023-04-04 06:24:52 +0000

    Update dist/ jsonify.py tests/fixtures/

--- gendiff/format/jsonify.py
+++ gendiff/format/jsonify.py
@@ -2,26 +2,30 @@ import json
 
 
 def render(diffed):
-    result = {}
+    return json.dumps(diffed)
 
-    def inner(data, dic):
-        for item in data:
-            if isinstance(item, tuple):
-                key, status, value = item
-                if isinstance(value, list) and status == 'changed':
-                    inner(value, dic)
-                elif status == 'nested':
-                    nested = inner(value, {})
-                    dic[key] = {'nested': nested}
-                elif isinstance(value, tuple) and status == 'changed':
-                    dic[key] = {
-                        'status': status,
-                        'update': {
-                            'removed': value[0],
-                            'added': value[1]
-                        }
-                    }
-                else:
-                    dic[key] = {'status': status, 'value': value}
-        return dic
-    return json.dumps(inner(diffed, result))
+
+# def new_render(diffed):
+#    result = {}
+
+#    def inner(data, dic):
+#        for item in data:
+#            if isinstance(item, tuple):
+#                key, status, value = item
+#                if isinstance(value, list) and status == 'changed':
+#                    inner(value, dic)
+#                elif status == 'nested':
+#                    nested = inner(value, {})
+#                    dic[key] = {'nested': nested}
+#                elif isinstance(value, tuple) and status == 'changed':
+#                    dic[key] = {
+#                        'status': status,
+#                        'update': {
+#                            'removed': value[0],
+#                            'added': value[1]
+#                        }
+#                    }
+#                else:
+#                    dic[key] = {'status': status, 'value': value}
+#        return dic
+#    return json.dumps(inner(diffed, result))
